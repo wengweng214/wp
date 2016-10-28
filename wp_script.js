0dc8d2c209283bb771202ae5c584da87ca083b26
wengweng214 <jenelporlas@gmail.com>
2016-10-28 22:30:08 +0800

    Create wp_script.js

---
+++ wp_script.js
@@ -0,0 +1,9 @@
+jQuery(function($) {
+    $(document).ready(function(){
+            $('#insert-my-media').click(open_media_window);
+        });
+
+    function open_media_window() {
+          alert('no function yet! it is still under construction');
+    }
+});
